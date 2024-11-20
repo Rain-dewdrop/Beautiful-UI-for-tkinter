b2302d5e2797c407592618bcdeb7fba16d7d761a
Rain_dewdrop <3350941618@qq.com>
2024-11-20 20:39:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ def uistyles(num):
 size = size = 500, 300
 root = tkt.Tk(title="test",size=size)
 root.center()
-uistyles(7)    #更改这里的数字来改变窗口风格   Change the numbers here to change the window style.
+uistyles(3)    #更改这里的数字来改变窗口风格   Change the numbers here to change the window style.
 root.mainloop()
